91ac265b50f37097b9f0a1a63d43cc8560ccd127
Olav <olav.hendrickx@hotmail.com>
2019-06-09 16:13:26 +0200

    Added race repositories and create queries

---
+++ Racing.BL/Models/Race.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Racing.BL.Models
+{
+    public class Race
+    {
+        private readonly Guid raceId;
+        private readonly List<Driver> listOfParticipants;
+        private readonly Guid raceTrackId;
+
+        public Race(List<Driver> inputListOfParticipants, RaceTrack raceTrack)
+        {
+            raceId = Guid.NewGuid();
+            listOfParticipants = inputListOfParticipants;
+            raceTrackId = raceTrack.RaceTrackId;
+        }
+
+        #region Accessors
+        public Guid RaceId
+        {
+            get { return raceId; }
+        }
+
+        public List<Driver> ListOfParticipants
+        {
+            get { return listOfParticipants; }
+        }
+
+        public Guid RaceTrackId
+        {
+            get { return raceTrackId; }
+        }
+        #endregion
+    }
+}
