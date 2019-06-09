52ecc1526c2288d8f7a11d7330e2bdc67987e294
Olav <olav.hendrickx@hotmail.com>
2019-06-09 14:02:23 +0200

    Added page for a race

---
+++ Racing.UI.WPF/RaceEngine/Race.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Racing.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Racing.UI.WPF.RaceEngine
+{
+    /// <summary>
+    /// Interaction logic for Race.xaml
+    /// </summary>
+    public partial class Race : Page
+    {
+        List<Driver> listOfDrivers = new List<Driver>();
+        RaceTrack raceTrack = new RaceTrack();
+        public Race(List<Driver> inputListOfDrivers, RaceTrack inputRaceTrack)
+        {
+            InitializeComponent();
+
+            listOfDrivers = inputListOfDrivers;
+            raceTrack = inputRaceTrack;
+        }
+    }
+}
